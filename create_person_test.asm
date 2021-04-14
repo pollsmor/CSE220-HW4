42281c79ff803ca2512597d179c47a6e59caa11f
Kevin Li <kevinli4321@gmail.com>
2021-04-14 11:05:09 -0400

    Part 4 done

--- create_person_test.asm
+++ create_person_test.asm
@@ -9,16 +9,35 @@ Network:
   .word 0   #curr_num_of_edges (bytes 20 - 23)
   .asciiz "NAME" # Name property (bytes 24 - 28)
   .asciiz "FRIEND" # FRIEND property (bytes 29 - 35)
-   # nodes (bytes 36 - 95)	
+   # set of nodes (bytes 36 - 95)	
   .byte 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0	
    # set of edges (bytes 96 - 215)
   .word 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
 
 .text:
 main:
+	# See what is the initial address of network, compare with returned address of node
+	la $a0, Network	
+	li $v0, 1
+	syscall
+	li $a0, '\n'
+	li $v0, 11
+	syscall
+
 	la $a0, Network
 	jal create_person
 	#write test code
+	move $a0, $v0		# $v0 return value
+	li $v0, 1
+	syscall
+	li $a0, '\n'
+	li $v0, 11
+	syscall
+
+	la $a0, Network		# Checking curr_num_of_nodes
+	lw $a0, 16($a0)
+	li $v0, 1
+	syscall
 	
 	li $v0, 10
 	syscall
--- hw4.asm
+++ hw4.asm
@@ -53,6 +53,23 @@ str_cpy:
 	jr $ra
 	
 create_person:
+	lw $t0, 0($a0)		# Total nodes
+	lw $t1, 16($a0)		# Current nodes	
+	blt $t1, $t0, nodeAvailable
+	# No nodes available
+	li $v0, -1
+	j return_create_person
+	
+	nodeAvailable:
+	addi $v0, $a0, 36	# Return value is address of node, and set of nodes begins at byte 36
+	li $t0, 12
+	mult $t1, $t0		# Multiply current nodes by 12 as each node takes up that much space
+	mflo $t0		# Now contains offset for set of nodes
+	add $v0, $v0, $t0
+	addi $t1, $t1, 1	# Increment current nodes
+	sw $t1, 16($a0)
+	
+	return_create_person:
 	jr $ra
 	
 is_person_exists:
