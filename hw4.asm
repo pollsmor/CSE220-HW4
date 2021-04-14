6787b6db2d999f9f1577546d1de42b209ff64e35
Kevin Li <kevinli4321@gmail.com>
2021-04-13 21:29:56 -0400

    Part 1 done

--- hw4.asm
+++ hw4.asm
@@ -4,26 +4,47 @@
 .text:
 
 str_len:
+	li $v0, 0	# Not gonna modify $v0 so may as well increment and return it directly
+	len_loop:
+		lbu $t0, 0($a0)
+		beq $t0, $0, return_str_len
+		
+		addi $v0, $v0, 1
+		addi $a0, $a0, 1
+		j len_loop
+
+	return_str_len:
 	jr $ra
+	
 str_equals:
 	jr $ra
+	
 str_cpy:
 	jr $ra
+	
 create_person:
 	jr $ra
+	
 is_person_exists:
 	jr $ra
+	
 is_person_name_exists:
 	jr $ra
+	
 add_person_property:
 	jr $ra
+	
 get_person:
 	jr $ra
+	
 is_relation_exists:
 	jr $ra
+	
 add_relation:
 	jr $ra
+	
 add_relation_property:
 	jr $ra
+	
 is_friend_of_friend:
 	jr $ra
--- str_len_test.asm
+++ str_len_test.asm
@@ -7,6 +7,10 @@ main:
 	la $a0, str1
 	jal str_len
 	#write test code
+	move $a0, $v0
+	li $v0, 1
+	syscall
+	
 	li $v0, 10
 	syscall
 	
